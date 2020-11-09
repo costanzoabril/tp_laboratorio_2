3dbabfc3bfee5da5f72115b89505697bcc9689a4
abrilcostanzo <costanzoabril@gmail.com>
2020-11-09 19:50:58 -0300

    recutp1

---
+++ RecuperatoriosTP/TP1/MiCalculadora/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MiCalculadora
+{
+    partial class FormCalculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNum1 = new System.Windows.Forms.TextBox();
+            this.comboBoxOperador = new System.Windows.Forms.ComboBox();
+            this.textBoxNum2 = new System.Windows.Forms.TextBox();
+            this.bOperar = new System.Windows.Forms.Button();
+            this.bLimpiar = new System.Windows.Forms.Button();
+            this.bCerrar = new System.Windows.Forms.Button();
+            this.bABinario = new System.Windows.Forms.Button();
+            this.bADecimal = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxNum1
+            // 
+            this.textBoxNum1.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNum1.Location = new System.Drawing.Point(12, 67);
+            this.textBoxNum1.Name = "textBoxNum1";
+            this.textBoxNum1.Size = new System.Drawing.Size(132, 35);
+            this.textBoxNum1.TabIndex = 0;
+            // 
+            // comboBoxOperador
+            // 
+            this.comboBoxOperador.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOperador.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxOperador.FormattingEnabled = true;
+            this.comboBoxOperador.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBoxOperador.Location = new System.Drawing.Point(152, 68);
+            this.comboBoxOperador.Name = "comboBoxOperador";
+            this.comboBoxOperador.Size = new System.Drawing.Size(132, 34);
+            this.comboBoxOperador.TabIndex = 1;
+            // 
+            // textBoxNum2
+            // 
+            this.textBoxNum2.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNum2.Location = new System.Drawing.Point(290, 67);
+            this.textBoxNum2.Name = "textBoxNum2";
+            this.textBoxNum2.Size = new System.Drawing.Size(132, 35);
+            this.textBoxNum2.TabIndex = 3;
+            // 
+            // bOperar
+            // 
+            this.bOperar.Location = new System.Drawing.Point(12, 120);
+            this.bOperar.Name = "bOperar";
+            this.bOperar.Size = new System.Drawing.Size(132, 47);
+            this.bOperar.TabIndex = 4;
+            this.bOperar.Text = "Operar";
+            this.bOperar.UseVisualStyleBackColor = true;
+            this.bOperar.Click += new System.EventHandler(this.bOperar_Click);
+            // 
+            // bLimpiar
+            // 
+            this.bLimpiar.Location = new System.Drawing.Point(152, 120);
+            this.bLimpiar.Name = "bLimpiar";
+            this.bLimpiar.Size = new System.Drawing.Size(132, 47);
+            this.bLimpiar.TabIndex = 5;
+            this.bLimpiar.Text = "Limpiar";
+            this.bLimpiar.UseVisualStyleBackColor = true;
+            this.bLimpiar.Click += new System.EventHandler(this.bLimpiar_Click);
+            // 
+            // bCerrar
+            // 
+            this.bCerrar.Location = new System.Drawing.Point(290, 120);
+            this.bCerrar.Name = "bCerrar";
+            this.bCerrar.Size = new System.Drawing.Size(132, 47);
+            this.bCerrar.TabIndex = 6;
+            this.bCerrar.Text = "Cerrar";
+            this.bCerrar.UseVisualStyleBackColor = true;
+            this.bCerrar.Click += new System.EventHandler(this.bCerrar_Click);
+            // 
+            // bABinario
+            // 
+            this.bABinario.Location = new System.Drawing.Point(12, 173);
+            this.bABinario.Name = "bABinario";
+            this.bABinario.Size = new System.Drawing.Size(202, 48);
+            this.bABinario.TabIndex = 7;
+            this.bABinario.Text = "Convertir a binario";
+            this.bABinario.UseVisualStyleBackColor = true;
+            this.bABinario.Click += new System.EventHandler(this.bABinario_Click);
+            // 
+            // bADecimal
+            // 
+            this.bADecimal.Location = new System.Drawing.Point(220, 173);
+            this.bADecimal.Name = "bADecimal";
+            this.bADecimal.Size = new System.Drawing.Size(202, 48);
+            this.bADecimal.TabIndex = 8;
+            this.bADecimal.Text = "Convertir a decimal";
+            this.bADecimal.UseVisualStyleBackColor = true;
+            this.bADecimal.Click += new System.EventHandler(this.bADecimal_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 25);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblResultado.Size = new System.Drawing.Size(410, 39);
+            this.lblResultado.TabIndex = 2;
+            // 
+            // FormCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 234);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.bADecimal);
+            this.Controls.Add(this.bABinario);
+            this.Controls.Add(this.bCerrar);
+            this.Controls.Add(this.bLimpiar);
+            this.Controls.Add(this.bOperar);
+            this.Controls.Add(this.textBoxNum2);
+            this.Controls.Add(this.comboBoxOperador);
+            this.Controls.Add(this.textBoxNum1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Abril Costanzo del curso 2°A";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormCalculadora_FormClosing);
+            this.Load += new System.EventHandler(this.FormCalculadora_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNum1;
+        private System.Windows.Forms.ComboBox comboBoxOperador;
+        private System.Windows.Forms.TextBox textBoxNum2;
+        private System.Windows.Forms.Button bOperar;
+        private System.Windows.Forms.Button bLimpiar;
+        private System.Windows.Forms.Button bCerrar;
+        private System.Windows.Forms.Button bABinario;
+        private System.Windows.Forms.Button bADecimal;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ RecuperatoriosTP/TP1/MiCalculadora/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        /// <summary>
+        /// Toma los valores y el operador elegidos por el usuario y muestra su resultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bOperar_Click(object sender, EventArgs e)
+        {
+            string op = this.comboBoxOperador.Text;
+            string num1 = this.textBoxNum1.Text;
+            string num2 = this.textBoxNum2.Text;
+
+            this.lblResultado.Text = (Operar(num1, num2, op)).ToString();
+        }
+
+        /// <summary>
+        /// Llama a Calculadora.Operar() y devuelve el resultado de la operacion
+        /// </summary>
+        /// <param name="numero1"></param>
+        /// <param name="numero2"></param>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        private static double Operar(string num1, string num2, string op)
+        {
+            Numero aux1 = new Numero(num1);
+            Numero aux2 = new Numero(num2);
+
+            return Calculadora.Operar(aux1, aux2, op);
+        }
+
+        /// <summary>
+        /// Llama al Limpiar()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        /// <summary>
+        /// Vacia todos los textBox y el comboBox
+        /// </summary>
+        private void Limpiar()
+        {
+            this.comboBoxOperador.Text = string.Empty;
+            this.textBoxNum1.Clear();
+            this.textBoxNum2.Clear();         
+            this.lblResultado.Text = "";
+        }
+
+        /// <summary>
+        /// Cierra el formulario.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Convierte el resultado a binario (de ser posible)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bABinario_Click(object sender, EventArgs e)
+        {
+            string resultDecimal = this.lblResultado.Text;
+            this.lblResultado.Text = Numero.DecimalBinario(resultDecimal);
+        }
+
+        /// <summary>
+        /// Convierte el resultado a decimal (de ser posible)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bADecimal_Click(object sender, EventArgs e)
+        {
+            string resultBinario = this.lblResultado.Text;
+            this.lblResultado.Text = Numero.BinarioDecimal(resultBinario);
+        }
+
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult rta = MessageBox.Show("¿Seguro de querer salir?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+
+            if(rta == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
